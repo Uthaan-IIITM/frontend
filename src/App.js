f99cc7e918240899b8281500e2b0f048c69fe860
Raj varsani <varasaniraj5@gmail.com>
2021-08-19 13:06:25 +0530

    Fomatted app.js file
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import HomeUpperSection from "./components/home/upper-section/HomeUpperSection";
 import "./styles/App.css";
 
 function App() {
+
+  // This code is Just for Testing purposes
+  // Actual code will contain routes and stufff...
+
   return <div className="App">
     <HomeUpperSection/>
   </div>;
