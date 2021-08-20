7d02afd24cf58ef0da0615d7be42d9c631f58479
Raj varsani <varasaniraj5@gmail.com>
2021-08-20 13:37:21 +0530

    Passed opacity and scale controller props to slidr
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/HomeUpperSection.js
+++ src/components/home/upper-section/HomeUpperSection.js
@@ -3,15 +3,21 @@ import ElementsSlider from './ElementsSlider'
 import BridgingGapBetweenJuniorsAndSeniors from './illustration-components/BridgingGapBetweenJuniorsAndSeniors';
 import ToSeekToFindNotToYield from './illustration-components/ToSeekToFindNotToYield';
 import PushingBoundariesOnOurIdeasAndOurCraft from './illustration-components/PushingBoundariesOnOurIdeasAndOurCraft';
+import homePageSliderOpacityController from './helper-functions/home-page-slider-opacity-controller';
+import homePageSliderScaleController from './helper-functions/home-page-slider-scale-controller';
 
-const IllustionComponentsArray = [<BridgingGapBetweenJuniorsAndSeniors/>,
-    < ToSeekToFindNotToYield />,
-    < PushingBoundariesOnOurIdeasAndOurCraft />];
+
+
+const IllustionComponentsArray = [<BridgingGapBetweenJuniorsAndSeniors />,
+< ToSeekToFindNotToYield />,
+< PushingBoundariesOnOurIdeasAndOurCraft />];
 
 function HomeUpperSection() {
     return (
         <div >
-            <ElementsSlider sliderComponents={IllustionComponentsArray} />
+            <ElementsSlider sliderComponents={IllustionComponentsArray}
+            opacityFunction={homePageSliderOpacityController}
+            scaleFunction = {homePageSliderScaleController} />
         </div>
     )
 }
