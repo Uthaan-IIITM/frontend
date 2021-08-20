32fd9dc0e7c36984b5a8ab32582a52cdf60c65a8
Raj varsani <varasaniraj5@gmail.com>
2021-08-20 13:36:30 +0530

    Updated element slider >> Added opacity and scale function >> Added anims to core-image-wrapper
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/ElementsSlider.js
+++ src/components/home/upper-section/ElementsSlider.js
@@ -2,6 +2,9 @@ import PropTypes from 'prop-types'
 import { useState } from 'react/cjs/react.development'
 import "../../../styles/home/upper-section/slider/image-slider-container.css"
 import "../../../styles/home/upper-section/slider/core-image-slider-container.css"
+import homePageSliderOpacityController from './helper-functions/home-page-slider-opacity-controller';
+import homePageSliderScaleController from './helper-functions/home-page-slider-scale-controller';
+import formatNumber from './slider-helpers/nemerical-formatter';
 function ElementsSlider(
   {
     sliderComponents,
@@ -15,8 +18,12 @@ function ElementsSlider(
   }
 ) {
 
+  const [currentPositionFloatIndex, setCurrentPositionFloatIndex] = useState(0.00)
   const [currentPositionIndex, setCurrentPositionIndex] = useState(0)
+  const [sliderElementsOpacity, setSliderElementsOpacity] = useState([])
+  const [sliderElementsScale, setSliderElementsScale] = useState([])
   const [beingDragged, setBeingDragged] = useState(false)
+
 
 
   /** function to drag carousel when user is dragging **/
@@ -36,12 +43,68 @@ function ElementsSlider(
     // console.log(e.target.scrollLeft)
     // console.log(e.target.offsetWidth )
 
-    let tempElee = e.target.childNodes[0].childNodes[0].childNodes[0].style
+    let sliderfirstElement = childElements[0]
+    let sliderfirstElementComputedStyles = getComputedStyle(sliderfirstElement)
 
-    let centerPositionAtZeroScroll = carouselWidth / 2 - elementWidth / 2 + spaceBetween
+    // console.log(tempElee.getBoundingClientRect());
+    // console.log(Number(tempEleeComputedStyles.width )+ tempEleeComputedStyles.marginLeft);
+    // console.log(parseInt(tempEleeComputedStyles.marginLeft));
+
+
+
+
+    let localCurrentPositionIndex = (e.target.scrollLeft / (parseFloat(sliderfirstElementComputedStyles.width) + 2 * parseFloat(sliderfirstElementComputedStyles.marginLeft)))
+
+    setCurrentPositionFloatIndex(formatNumber(localCurrentPositionIndex, 2))
+    setCurrentPositionIndex(formatNumber(localCurrentPositionIndex, 0))
+    // console.log(currentPositionFloatIndex);
+    // console.log(currentPositionIndex);
+
+    let maxIndexForScaleAndOpacity = Math.max(childElements.length - currentPositionIndex, 1 + parseInt(currentPositionIndex)) + 1;
+
+    let localeArrayForOpacityCalculation = [];
+    let localeArrayForScaleCalculation = [];
+    for (let i = 0; i < maxIndexForScaleAndOpacity; i++) {
+      localeArrayForOpacityCalculation.push(opacityFunction(i))
+      localeArrayForScaleCalculation.push(scaleFunction(i))
+    }
+
+    setSliderElementsOpacity(localeArrayForOpacityCalculation)
+    setSliderElementsScale(localeArrayForScaleCalculation)
+
+    console.log(sliderElementsOpacity);
+    console.log(sliderElementsScale);
+
+
+    let comparisonIndex = 0;
+    let comparisonRatio = 0;
+    if (currentPositionFloatIndex > currentPositionIndex) {
+      comparisonIndex = 1;
+      comparisonRatio = currentPositionFloatIndex - currentPositionIndex;
+    } else if (currentPositionFloatIndex < currentPositionIndex) {
+      comparisonIndex = - 1;
+      comparisonRatio = currentPositionIndex - currentPositionFloatIndex;
+    }
+
     for (let i = 0; i < childElements.length; i++) {
       // let tempWidth = `${Math.floor((Math.random() * 100) + 1)}%`;
-      // childElements[i].childNodes[0].style.width = tempWidth;
+
+      let currentIndexElementOpacity = sliderElementsOpacity[Math.abs(i - currentPositionIndex)]
+      let currentIndexElementWidth = sliderElementsScale[Math.abs(i - currentPositionIndex)]
+
+      let opacityToBeSet = currentIndexElementOpacity;
+      let scaleToBeSet = currentIndexElementWidth;
+
+      if (comparisonIndex != 0) {
+
+        let comparisonElementIndex  = 
+
+        opacityToBeSet = opacityFunction(i + comparisonIndex)
+        scaleToBeSet = scaleFunction(i + comparisonIndex)
+      }
+      
+      childElements[i].childNodes[0].style.width = `${100 * parseFloat(currentIndexElementWidth)}%`;
+      childElements[i].childNodes[0].style.opacity = currentIndexElementOpacity;
     }
 
   }
@@ -52,6 +115,8 @@ function ElementsSlider(
       onScroll={(e) => updateLayoutOnScroll(e)}
       style={{
         width: carouselWidth,
+        paddinTop: paddingGeneral,
+        padduBottom: paddingGeneral,
       }}
     >
       <div className="slider-elements-wrapper-wrapper"
@@ -65,7 +130,6 @@ function ElementsSlider(
         <div className="slider-elements-wrapper"
 
           style={{
-
             marginLeft: `calc( (${carouselWidth} /2 - ${elementWidth}/ 2) + ${spaceBetween} )`,
             marginRight: `calc( (${carouselWidth} /2 - ${elementWidth}/ 2) + ${spaceBetween} )`,
           }}
@@ -109,10 +173,14 @@ ElementsSlider.propTypes = {
 
 ElementsSlider.defaultProps = {
   spaceBetween: "5vw",
-  paddingGeneral: "45vw",
+  paddingGeneral: "10vw",
   alignment: "center",
   carouselWidth: "100vw",
   elementWidth: "40rem",
+  opacityFunction: { homePageSliderOpacityController },
+  scaleFunction: { homePageSliderScaleController }
 }
 
 export default ElementsSlider
+
+
