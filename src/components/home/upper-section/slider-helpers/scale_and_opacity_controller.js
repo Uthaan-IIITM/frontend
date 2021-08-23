15d268b6b496c60700e800642ff7b228b0b4211f
Raj varsani <varasaniraj5@gmail.com>
2021-08-23 12:36:46 +0530

    Created scale_and_opacity_controller.js for  opacity and scale manipulation in ElementSlider.js
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/slider-helpers/scale_and_opacity_controller.js
+++ src/components/home/upper-section/slider-helpers/scale_and_opacity_controller.js
@@ -1,9 +1,67 @@
-const formatNumber = (number, accuracy = 2) => {
-    const formatterToAccuracy = new Intl.NumberFormat('en-US', {
-        minimumFractionDigits: accuracy,
-        maximumFractionDigits: accuracy,
-    });
-    return formatterToAccuracy.format(number)
+const scaleAndOpacityController = (currentPositionIndex, sliderElements, opacityValues, sclaleValues) => {
+      
 }
 
-export default formatNumber
+export default scaleAndOpacityController
+
+
+
+
+
+// let comparisonIndex = 0;
+// let comparisonRatio = 0.00;
+// if (currentPositionFloatIndex > currentPositionIndex) {
+//   comparisonIndex = 1;
+//   comparisonRatio = currentPositionFloatIndex - currentPositionIndex;
+// } else if (currentPositionFloatIndex < currentPositionIndex) {
+//   comparisonIndex = - 1;
+//   comparisonRatio = currentPositionIndex - currentPositionFloatIndex;
+// } else {
+//   comparisonIndex = 0;
+//   comparisonRatio = 0;
+// }
+
+// // console.log(comparisonRatio);
+// // console.log(comparisonIndex);
+
+// for (let i = 0; i < childElements.length; i++) {
+//   // let tempWidth = `${Math.floor((Math.random() * 100) + 1)}%`;
+
+//   let currentElementIndex
+//   if (i - currentPositionIndex > 0) {
+//     currentElementIndex = i - currentPositionIndex
+//   } else {
+//     currentElementIndex = currentPositionIndex - i
+//     comparisonIndex = -comparisonIndex
+//   }
+
+
+//   console.log(currentElementIndex)
+
+//   let currentIndexElementOpacity = sliderElementsOpacity[currentElementIndex]
+//   let currentIndexElementWidth = sliderElementsScale[currentElementIndex]
+
+//   let opacityToBeSet = currentIndexElementOpacity;
+//   let scaleToBeSet = currentIndexElementWidth;
+
+
+
+//   if (comparisonIndex != 0) {
+
+//     let comparisonElementIndex = currentElementIndex + comparisonIndex;
+
+
+//     let comparisonIndexElementOpacity = sliderElementsOpacity[comparisonElementIndex]
+//     let comparisonIndexElementWidth = sliderElementsScale[comparisonElementIndex]
+
+//     opacityToBeSet = (1.00 - comparisonRatio) * parseFloat(currentIndexElementOpacity) + parseFloat(comparisonRatio) * parseFloat(comparisonIndexElementOpacity)
+//     scaleToBeSet = (1.00 - comparisonRatio) * parseFloat(currentIndexElementWidth) + parseFloat(comparisonRatio) * parseFloat(comparisonIndexElementWidth)
+
+//     console.log(comparisonRatio);
+//     // console.log(opacityToBeSet);
+//     // console.log(scaleToBeSet);
+//   }
+
+//   childElements[i].childNodes[0].style.width = `${100 * parseFloat(scaleToBeSet)}%`;
+//   // childElements[i].childNodes[0].style.opacity = opacityToBeSet;
+// }
