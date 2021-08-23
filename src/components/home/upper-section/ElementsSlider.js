ac288813c56fc4fbae7db3761f0f4b6590cf1527
Raj varsani <varasaniraj5@gmail.com>
2021-08-23 18:06:14 +0530

    Updated ElementSlider.js >> Updated elements maniplation functions >> Updated hooks >> Updated usestates
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/ElementsSlider.js
+++ src/components/home/upper-section/ElementsSlider.js
@@ -1,105 +1,134 @@
-import PropTypes from 'prop-types'
-import { useState } from 'react/cjs/react.development'
-import "../../../styles/home/upper-section/slider/image-slider-container.css"
-import "../../../styles/home/upper-section/slider/core-image-slider-container.css"
-import homePageSliderOpacityController from './helper-functions/home-page-slider-opacity-controller';
-import homePageSliderScaleController from './helper-functions/home-page-slider-scale-controller';
-import formatNumber from './slider-helpers/nemerical-formatter';
-function ElementsSlider(
-  {
-    sliderComponents,
-    spaceBetween,
-    paddingGeneral,
-    alignment,
-    elementWidth,
-    carouselWidth,
-    opacityFunction,
-    scaleFunction
-  }
-) {
+import PropTypes from "prop-types";
+import { useState } from "react/cjs/react.development";
+import "../../../styles/home/upper-section/slider/image-slider-container.css";
+import "../../../styles/home/upper-section/slider/core-image-slider-container.css";
+import homePageSliderOpacityController from "./helper-functions/home-page-slider-opacity-controller";
+import homePageSliderScaleController from "./helper-functions/home-page-slider-scale-controller";
+import formatNumber from "./slider-helpers/nemerical-formatter";
+import scaleAndOpacityControllerByActualPosition from "./slider-helpers/scale_and_opacity_controller_by_actual_position";
+import scaleAndOpacityControllerByPositionFromCenter from "./slider-helpers/scale_and_opacity_controller_by_position_from_center";
+import scaleAndOpacitySetter from "./slider-helpers/scale_and_opacity_setter";
+import { useEffect } from "react";
 
-  const [currentPositionFloatIndex, setCurrentPositionFloatIndex] = useState(0.00)
-  const [currentPositionIndex, setCurrentPositionIndex] = useState(0)
-  const [sliderElementsOpacity, setSliderElementsOpacity] = useState([])
-  const [sliderElementsScale, setSliderElementsScale] = useState([])
-  const [beingDragged, setBeingDragged] = useState(false)
-  const [lastTimeBeingAnimated, setLastTimeBeingAnimated] = useState(Date.now())
+function ElementsSlider({
+  sliderComponents,
+  spaceBetween,
+  paddingGeneral,
+  alignment,
+  elementWidth,
+  carouselWidth,
+  opacityFunction,
+  scaleFunction,
+  onScrollChangeFunction,
+}) {
+  const [sliderChildElements, setSliderChildElements] = useState([]);
+  const [opacityAndScaleDimensions, setOpacityAndScaleDimensions] = useState(
+    {}
+  );
+  const [currentScrollStateIndices, setCurrentScrollStateIndices] = useState(
+    {}
+  );
+  const [beingDragged, setBeingDragged] = useState(false);
+  const [lastTimeBeingAnimated, setLastTimeBeingAnimated] = useState(
+    Date.now()
+  );
 
+  useEffect(() => {
+    if (onScrollChangeFunction !== undefined) {
+      onScrollChangeFunction(
+        currentScrollStateIndices.floatIndex,
+        currentScrollStateIndices.intIndex
+      );
+    }
 
+    let [
+      localeArrayForOpacityCalculationByPositionFromCenter,
+      localeArrayForScaleCalculationByPositionFromCenter,
+    ] = scaleAndOpacityControllerByPositionFromCenter(
+      sliderChildElements.length,
+      currentScrollStateIndices.intIndex,
+      opacityFunction,
+      scaleFunction
+    );
+
+    let [
+      localeArrayForOpacityCalculationByActualPosition,
+      localeArrayForScaleCalculationByActualPosition,
+    ] = scaleAndOpacityControllerByActualPosition(
+      currentScrollStateIndices.intIndex,
+      currentScrollStateIndices.floatIndex,
+      sliderChildElements.length,
+      localeArrayForOpacityCalculationByPositionFromCenter,
+      localeArrayForScaleCalculationByPositionFromCenter
+    );
+
+    let localeDimensionsObject = {};
+    localeDimensionsObject.opacityDimensionsByPositionFromCenter =
+      localeArrayForOpacityCalculationByPositionFromCenter;
+    localeDimensionsObject.opacityDimensionsByActualPosition =
+      localeArrayForOpacityCalculationByActualPosition;
+    localeDimensionsObject.scaleDimensionsByPositionFromCenter =
+      localeArrayForScaleCalculationByPositionFromCenter;
+    localeDimensionsObject.scaleDimensionsByActualPosition =
+      localeArrayForScaleCalculationByActualPosition;
+
+    setOpacityAndScaleDimensions(localeDimensionsObject);
+  }, [currentScrollStateIndices]);
+
+  useEffect(() => {
+    scaleAndOpacitySetter(
+      sliderChildElements,
+      opacityAndScaleDimensions.scaleDimensionsByActualPosition,
+      opacityAndScaleDimensions.opacityDimensionsByActualPosition
+    );
+  }, [opacityAndScaleDimensions]);
 
   /** function to drag carousel when user is dragging **/
   function dragCarousel(event) {
     if (beingDragged) {
-      const userMousePos = event.clientX
-      console.log(userMousePos)
+      const userMousePos = event.clientX;
+      console.log(userMousePos);
     }
   }
 
   function updateLayoutOnScroll(e) {
+    let childElements = e.target.childNodes[0].childNodes[0].childNodes;
+    setSliderChildElements(childElements);
 
-    // if (lastTimeBeingAnimated + 100 > Date.now()) {
-    //   return
-    // }
-    // setLastTimeBeingAnimated(Date.now())
+    let sliderfirstElement = childElements[0];
+    let sliderfirstElementComputedStyles = getComputedStyle(sliderfirstElement);
 
-    // console.log(e.target.childNodes[0].childNodes[0].childNodes)
-    let childElements = e.target.childNodes[0].childNodes[0].childNodes
-    // for (let i = 0; i < childElements.length; i++) {
-    //   console.log(childElements[i].offsetLeft)
-    // }
-    // console.log(e.target.scrollLeft)
-    // console.log(e.target.offsetWidth )
+    let localCurrentPositionIndex =
+      e.target.scrollLeft /
+      (parseFloat(sliderfirstElementComputedStyles.width) +
+        2 * parseFloat(sliderfirstElementComputedStyles.marginLeft));
 
-    let sliderfirstElement = childElements[0]
-    let sliderfirstElementComputedStyles = getComputedStyle(sliderfirstElement)
+    let localeCurrentScrollStateIndices = {};
+    localeCurrentScrollStateIndices.floatIndex = formatNumber(
+      localCurrentPositionIndex,
+      2
+    );
+    localeCurrentScrollStateIndices.intIndex = formatNumber(
+      localCurrentPositionIndex,
+      0
+    );
 
-    // console.log(tempElee.getBoundingClientRect());
-    // console.log(Number(tempEleeComputedStyles.width )+ tempEleeComputedStyles.marginLeft);
-    // console.log(parseInt(tempEleeComputedStyles.marginLeft));
-
-
-
-
-    let localCurrentPositionIndex = (e.target.scrollLeft / (parseFloat(sliderfirstElementComputedStyles.width) + 2 * parseFloat(sliderfirstElementComputedStyles.marginLeft)))
-
-    setCurrentPositionFloatIndex(formatNumber(localCurrentPositionIndex, 2))
-    setCurrentPositionIndex(formatNumber(localCurrentPositionIndex, 0))
-    // console.log(currentPositionFloatIndex);
-    // console.log(currentPositionIndex);
-
-    let maxIndexForScaleAndOpacity = Math.max(childElements.length - currentPositionIndex, 1 + parseInt(currentPositionIndex)) + 1;
-
-    let localeArrayForOpacityCalculation = [];
-    let localeArrayForScaleCalculation = [];
-    for (let i = 0; i < maxIndexForScaleAndOpacity; i++) {
-      localeArrayForOpacityCalculation.push(opacityFunction(i))
-      localeArrayForScaleCalculation.push(scaleFunction(i))
-    }
-
-    setSliderElementsOpacity(localeArrayForOpacityCalculation)
-    setSliderElementsScale(localeArrayForScaleCalculation)
-
-    console.log(sliderElementsOpacity);
-    console.log(sliderElementsScale);
-
-
-  
-
+    setCurrentScrollStateIndices(localeCurrentScrollStateIndices);
   }
 
-
   return (
-    <div className="slider-elements-wrapper-wrapper-wrapper"
+    <div
+      className="slider-elements-wrapper-wrapper-wrapper"
       onScroll={(e) => updateLayoutOnScroll(e)}
       style={{
         width: carouselWidth,
         paddinTop: paddingGeneral,
         padduBottom: paddingGeneral,
         height: "60vh",
-
       }}
     >
-      <div className="slider-elements-wrapper-wrapper"
+      <div
+        className="slider-elements-wrapper-wrapper"
         // onMouseDown={() => setBeingDragged(true)}
         // onMouseUp={() => setBeingDragged(false)}
         // onMouseMove={dragCarousel(e)}
@@ -107,13 +136,12 @@ function ElementsSlider(
           width: "fit-content",
         }}
       >
-        <div className="slider-elements-wrapper"
-
+        <div
+          className="slider-elements-wrapper"
           style={{
             marginLeft: `calc( (${carouselWidth} /2 - ${elementWidth}/ 2) + ${spaceBetween} )`,
             marginRight: `calc( (${carouselWidth} /2 - ${elementWidth}/ 2) + ${spaceBetween} )`,
           }}
-
         >
           {sliderComponents.map((component, index) => {
             return (
@@ -136,12 +164,12 @@ function ElementsSlider(
                   {component}
                 </div>
               </div>
-            )
+            );
           })}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 ElementsSlider.propTypes = {
@@ -149,18 +177,16 @@ ElementsSlider.propTypes = {
   spaceBetween: PropTypes.string,
   paddingGeneral: PropTypes.string,
   alignment: PropTypes.string,
-}
+};
 
 ElementsSlider.defaultProps = {
   spaceBetween: "5vw",
   paddingGeneral: "10vw",
-  alignment: "center",
+  alignment: "flex-end",
   carouselWidth: "100vw",
   elementWidth: "40rem",
   opacityFunction: { homePageSliderOpacityController },
-  scaleFunction: { homePageSliderScaleController }
-}
+  scaleFunction: { homePageSliderScaleController },
+};
 
-export default ElementsSlider
-
-
+export default ElementsSlider;
