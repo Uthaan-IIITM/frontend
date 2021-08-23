26c684be227240bc97bfb91cdd22fb89cc15a12d
Raj varsani <varasaniraj5@gmail.com>
2021-08-23 12:13:22 +0530

    Renamed imports for ToSeekToFindNotTo in homeuppersecYield to CreateConnectAndTrancend
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/HomeUpperSection.js
+++ src/components/home/upper-section/HomeUpperSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ElementsSlider from './ElementsSlider'
 import BridgingGapBetweenJuniorsAndSeniors from './illustration-components/BridgingGapBetweenJuniorsAndSeniors';
-import ToSeekToFindNotToYield from './illustration-components/ToSeekToFindNotToYield';
+import CreateConnectAndTrancend from './illustration-components/CreateConnectAndTrancend';
 import PushingBoundariesOnOurIdeasAndOurCraft from './illustration-components/PushingBoundariesOnOurIdeasAndOurCraft';
 import homePageSliderOpacityController from './helper-functions/home-page-slider-opacity-controller';
 import homePageSliderScaleController from './helper-functions/home-page-slider-scale-controller';
@@ -9,10 +9,10 @@ import homePageSliderScaleController from './helper-functions/home-page-slider-s
 
 
 const IllustionComponentsArray = [<BridgingGapBetweenJuniorsAndSeniors />,
-< ToSeekToFindNotToYield />,
-< ToSeekToFindNotToYield />,
-< ToSeekToFindNotToYield />,
-< ToSeekToFindNotToYield />,
+< CreateConnectAndTrancend />,
+< CreateConnectAndTrancend />,
+< CreateConnectAndTrancend />,
+< CreateConnectAndTrancend />,
 < PushingBoundariesOnOurIdeasAndOurCraft />];
 
 function HomeUpperSection() {
