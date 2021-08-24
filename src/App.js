99ca39f1737363650460b39f5050ca4602689339
Raj varsani <varasaniraj5@gmail.com>
2021-08-24 22:26:28 +0530

    Added temp routes switch for graphc anims
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/App.css";
 import Navbar from "./components/_general/Navbar";
 import HomePage from "./components/home/HomePage";
+import BridgingGapBetweenJuniorsAndSeniorsCopy from './components/home/upper-section/illustration-components/BridgingGapBetweenJuniorsAndSeniors_copy';
+import CreateConnectAndTrancendCopy from './components/home/upper-section/illustration-components/CreateConnectAndTrancend_copy';
+import PushingBoundariesOnOurIdeasAndOurCraftCopy from './components/home/upper-section/illustration-components/PushingBoundariesOnOurIdeasAndOurCraft_copy';
 
 function App() {
   // This code is Just for Testing purposes
@@ -13,6 +16,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/123" component={BridgingGapBetweenJuniorsAndSeniorsCopy} />
+          <Route exact path="/234" component={CreateConnectAndTrancendCopy} />
+          <Route exact path="/345" component={PushingBoundariesOnOurIdeasAndOurCraftCopy} />
         </Switch>
       </Router>
     </div>
