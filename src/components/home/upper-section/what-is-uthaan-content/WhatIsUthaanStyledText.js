20f876f764e611d91ca78699a4311640cbd91270
Raj varsani <varasaniraj5@gmail.com>
2021-08-24 19:38:28 +0530

    Added default value to currentstate prop in WhatIsUthaanStyledText >> Removed useEffect setinterval
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/what-is-uthaan-content/WhatIsUthaanStyledText.js
+++ src/components/home/upper-section/what-is-uthaan-content/WhatIsUthaanStyledText.js
@@ -3,22 +3,12 @@ import ReactTextTransition, { presets } from "react-text-transition";
 
 import "../../../../styles/home/upper-section/what-is-uthaan-content/WhatIsUthaanStyledText.css";
 
-function WhatIsUthaanStyledText({ currentState }) {
-  const [styleTextContentIndex, setStyleTextContentIndex] = useState(0);
+function WhatIsUthaanStyledText({ currentState = 0 }) {
   const StyleTextContent = [
     "ALorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus, quis dolor sit amet, consectetur adipiscing elit nunc tempus",
     "BLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus, quis dolor sit amet, consectetur adipiscing elit nunc tempus",
     "CLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus, quis dolor sit amet, consectetur adipiscing elit nunc tempus",
   ];
-
-  useEffect(() => {
-    let tempvar = 0;
-    setInterval(() => {
-      tempvar = tempvar + 1;
-      setStyleTextContentIndex(tempvar);
-      console.log(tempvar);
-    }, 4000);
-  }, []);
 
   return (
     <div>
