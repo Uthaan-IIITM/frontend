87b5e19fb383f8f837bee65bc346f2abe48183d5
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-24 17:49:11 +0530

    Created WHatisUthaanstyled Component >> imported react text transition
    Co-authored-by: Raj Varsani <RajVarsani@users.noreply.github.com>

---
+++ src/components/home/upper-section/what-is-uthaan-content/WhatIsUthaanStyledText.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import ReactTextTransition, { presets } from "react-text-transition";
+
+import "../../../../styles/home/upper-section/what-is-uthaan-content/WhatIsUthaanStyledText.css";
+
+function WhatIsUthaanStyledText() {
+  const [styleTextContentIndex, setStyleTextContentIndex] = useState(0);
+  const StyleTextContent = [
+    "ALorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus, quis dolor sit amet, consectetur adipiscing elit nunc tempus",
+    "BLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus, quis dolor sit amet, consectetur adipiscing elit nunc tempus",
+    "CLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus, quis dolor sit amet, consectetur adipiscing elit nunc tempus",
+  ];
+
+  useEffect(() => {
+    let tempvar = 0;
+    setInterval(() => {
+      tempvar = tempvar + 1;
+      setStyleTextContentIndex(tempvar);
+      console.log(tempvar);
+    }, 4000);
+  }, []);
+
+  return (
+    <div>
+      <div className="what-is-uthaan-styled-text">
+        <ReactTextTransition
+          text={
+            StyleTextContent[styleTextContentIndex % StyleTextContent.length]
+          }
+          noOverflow
+          springConfig={presets.wobbly}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default WhatIsUthaanStyledText;
