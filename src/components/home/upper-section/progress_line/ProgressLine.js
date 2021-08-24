6abefd51277388578bc5baf66e5fbc9aab36d6fb
Raj varsani <varasaniraj5@gmail.com>
2021-08-24 22:34:59 +0530

    Added repositioning of dots on state chnage
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/progress_line/ProgressLine.js
+++ src/components/home/upper-section/progress_line/ProgressLine.js
@@ -1,13 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 import "../../../../styles/home/upper-section/progress_line/progress_line.css";
 import LineDot from "./../../../_general/LineDot";
 
+import progressLibePositionController from "./helpers/position_conroller";
+
 function ProgressLine({ numberOfDots, currentState }) {
+  const positionSideDefiner = useRef([1, 1, 1]);
   const [currentStateForDot, setStateForCurrentDot] = useState([0, 1, 2]);
 
-  useEffect(() => {}, [currentStateForDot]);
+  useEffect(() => {
+    let tempStateObj = [];
+    let sideDefiner = [];
+    for (let index = 0; index < 3; index++) {
+      let tempState = index - currentState;
+      let tempSide = tempState > 0 ? 1 : -1;
+      tempStateObj.push(Math.abs(tempState));
+      sideDefiner.push(tempSide);
+    }
+    // console.log(tempStateObj);
+    // console.log(sideDefiner);
+    setStateForCurrentDot(tempStateObj);
+    positionSideDefiner.current = sideDefiner;
+  }, [currentState]);
+
+  useEffect(() => {
+    // let windowWidth = document.documentElement.clientWidth;
+    // console.log(windowWidth);
+    let windowWidth = window.innerWidth;
+    let elements = document.getElementsByClassName("line-dot-wrapper");
+    // console.log(windowWidth);
+    for (let index = 0; index < currentStateForDot.length; index++) {
+      let position;
+      let elementState = currentStateForDot[index];
+      if (!Number.isInteger(elementState)) {
+        let bottomState = Math.floor(elementState);
+        let topState = Math.ceil(elementState);
+        // console.log(elementState, bottomState , topState);
+        let positionIndexAccordingToBottomState =
+          progressLibePositionController(bottomState);
+        let positionIndexAccordingToTopState =
+          progressLibePositionController(topState);
+
+        position =
+          (positionIndexAccordingToBottomState * (topState - elementState) +
+            positionIndexAccordingToTopState * (elementState - bottomState)) /
+          2;
+
+        // console.log(position);
+      } else {
+        position = progressLibePositionController(elementState)/2;
+        console.log(position);
+      }
+      // console.log(
+      //   parseFloat(position) *
+      //     parseFloat(positionSideDefiner.current[index]) *
+      //     parseFloat(windowWidth) *
+      //     0.7
+      // );
+      elements[index].style.transform = `translateX(${
+        parseFloat(position) *
+        parseFloat(positionSideDefiner.current[index]) *
+        parseFloat(windowWidth) *
+        0.7
+      }px)`;
+    }
+  }, [currentStateForDot]);
 
   return (
     <div className="progress-line-wrapper">
