9d5400307881e07ecc9758bb79253d80fa54b99c
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-24 19:16:27 +0530

    Minor changes in style and dummy default state
    Co-authored-by: Raj Varsani <RajVarsani@users.noreply.github.com>

--- src/components/_general/LineDot.js
+++ src/components/_general/LineDot.js
@@ -11,8 +11,8 @@ function LineDot({
   opacityStates,
   currentState,
   transitionDuration,
+  // defaultState,
 }) {
-  currentState;
   return (
     <div
       className="dot-wrapper"
@@ -24,8 +24,8 @@ function LineDot({
       <div
         className="dot-outer-div"
         style={{
-          width: `calc( ${2 * outerRadiusStates[currentState]})`,
-          height: `calc( ${2 * outerRadiusStates[currentState]})`,
+          width: `calc( ${outerRadiusStates[currentState]}*2)`,
+          height: `calc( ${outerRadiusStates[currentState]}*2)`,
           borderRadius: `${outerRadiusStates[currentState]}`,
           opacity: opacityStates[currentState],
           borderWidth: `${strokeStates[currentState]}`,
@@ -36,8 +36,8 @@ function LineDot({
         <div
           className="dot-inner-div"
           style={{
-            width: `calc( ${2 * innerRadiusStates[currentState]})`,
-            height: `calc( ${2 * innerRadiusStates[currentState]})`,
+            width: `calc( ${innerRadiusStates[currentState]}*2)`,
+            height: `calc( ${innerRadiusStates[currentState]} * 2)`,
             borderRadius: `${innerRadiusStates[currentState]}`,
             opacity: opacityStates[currentState],
             backgroundColor: innerCircleColorStates[currentState],
@@ -55,17 +55,19 @@ LineDot.propTypes = {
   innerCircleColorStates: PropTypes.array,
   outerCircleColorStates: PropTypes.array,
   opacityStates: PropTypes.array,
+  // defaultState: PropTypes.number,
   currentState: PropTypes.number,
   transitionDuration: PropTypes.number,
 };
 
 LineDot.defaultProps = {
-  innerRadiusStates: ["2vw", "1vw", "0.8vw"],
-  outerRadiusStates: ["3vw", "1.5vw", "1.2vw"],
+  innerRadiusStates: ["0.5vw", "0.3vw", "0.1vw"],
+  outerRadiusStates: ["0.8vw", "0.6vw", "0.4vw"],
   innerCircleColorStates: ["#FFBC10", "#FFE39A", "#FFF0C9"],
   outerCircleColorStates: ["#222222", "#444444", "#888888"],
   strokeStates: ["3px", "1px", "1px"],
   opacityStates: [1, 1, 1],
+  // defaultState: 0,
   currentState: 0,
   transitionDuration: 200,
 };
