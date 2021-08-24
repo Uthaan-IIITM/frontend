42d128ee51084a07c1a08128de557e3c59745533
Raj varsani <varasaniraj5@gmail.com>
2021-08-24 15:07:13 +0530

    Formatted homeUpperSection.js file
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/HomeUpperSection.js
+++ src/components/home/upper-section/HomeUpperSection.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import ElementsSlider from "./slider/ElementsSlider"
-import BridgingGapBetweenJuniorsAndSeniors from './illustration-components/BridgingGapBetweenJuniorsAndSeniors';
-import CreateConnectAndTrancend from './illustration-components/CreateConnectAndTrancend';
-import PushingBoundariesOnOurIdeasAndOurCraft from './illustration-components/PushingBoundariesOnOurIdeasAndOurCraft';
-import homePageSliderOpacityController from './helper-functions/home-page-slider-opacity-controller';
-import homePageSliderScaleController from './helper-functions/home-page-slider-scale-controller';
+import React from "react";
+import ElementsSlider from "./slider/ElementsSlider";
+import BridgingGapBetweenJuniorsAndSeniors from "./illustration-components/BridgingGapBetweenJuniorsAndSeniors";
+import CreateConnectAndTrancend from "./illustration-components/CreateConnectAndTrancend";
+import PushingBoundariesOnOurIdeasAndOurCraft from "./illustration-components/PushingBoundariesOnOurIdeasAndOurCraft";
+import homePageSliderOpacityController from "./helper-functions/home-page-slider-opacity-controller";
+import homePageSliderScaleController from "./helper-functions/home-page-slider-scale-controller";
 
-
-
-const IllustionComponentsArray = [<BridgingGapBetweenJuniorsAndSeniors />,
-< CreateConnectAndTrancend />,
-< PushingBoundariesOnOurIdeasAndOurCraft />];
+const IllustionComponentsArray = [
+  <BridgingGapBetweenJuniorsAndSeniors />,
+  <CreateConnectAndTrancend />,
+  <PushingBoundariesOnOurIdeasAndOurCraft />,
+];
 
 function HomeUpperSection() {
-    return (
-        <div >
-            <ElementsSlider sliderComponents={IllustionComponentsArray}
-            opacityFunction={homePageSliderOpacityController}
-            scaleFunction = {homePageSliderScaleController} />
-        </div>
-    )
+  return (
+    <div>
+      <ElementsSlider
+        sliderComponents={IllustionComponentsArray}
+        opacityFunction={homePageSliderOpacityController}
+        scaleFunction={homePageSliderScaleController}
+      />
+    </div>
+  );
 }
 
-export default HomeUpperSection
+export default HomeUpperSection;
