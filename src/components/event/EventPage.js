b8e562e7b24887993751944f5d6fbcadd3299e82
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-28 22:36:27 +0530

    Create EventPage.js
    Co-Authored-By: Raj Varsani <74860406+RajVarsani@users.noreply.github.com>

---
+++ src/components/event/EventPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EventLowerSection from "./lower_section/EventLowerSection";
+
+function EventPage() {
+  return (
+    <div>
+      <EventLowerSection />
+    </div>
+  );
+}
+
+export default EventPage;
