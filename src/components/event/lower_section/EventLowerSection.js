600cab1450382711c32db31dd7b82978bc2c91b8
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-28 22:36:33 +0530

    Create EventLowerSection.js
    Co-Authored-By: Raj Varsani <74860406+RajVarsani@users.noreply.github.com>

---
+++ src/components/event/lower_section/EventLowerSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EventCard from "./EventCard";
+
+function EventLowerSection() {
+  return (
+    <div>
+      <EventCard />
+    </div>
+  );
+}
+
+export default EventLowerSection;
