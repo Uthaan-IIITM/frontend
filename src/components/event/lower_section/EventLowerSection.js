60a82146c7339953609f222774e3ed0ffa62dab6
Akash Gupta <akash.gupta.anup@gmail.com>
2021-08-29 14:44:50 +0530

    Update EventLowerSection.js
    Co-Authored-By: Raj Varsani <74860406+RajVarsani@users.noreply.github.com>

--- src/components/event/lower_section/EventLowerSection.js
+++ src/components/event/lower_section/EventLowerSection.js
@@ -1,10 +1,15 @@
 import React from "react";
+import LineDot from "../../_general/LineDot";
 import EventCard from "./EventCard";
 
 function EventLowerSection() {
   return (
     <div>
-      <EventCard />
+      <LineDot
+        innerRadiusStates={["1vw"]}
+        outerRadiusStates={["1.5vw"]}
+        innerCircleColorStates={["#303030"]}
+      />
     </div>
   );
 }
