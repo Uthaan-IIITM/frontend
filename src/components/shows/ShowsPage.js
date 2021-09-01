46b18bee3160c0b6da2fcedda2910be7cc7122aa
Raj varsani <varasaniraj5@gmail.com>
2021-09-01 20:44:24 +0530

    Added styling for text wrapper in shows page
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/shows/ShowsPage.js
+++ src/components/shows/ShowsPage.js
@@ -6,6 +6,10 @@ import { ReactComponent as Graphic3 } from "../../assets/shows/bg/bg3.svg";
 import { ReactComponent as Graphic4 } from "../../assets/shows/bg/bg4.svg";
 
 function ShowsPage() {
+  let divStyling = {
+    top: "21vh",
+    left: "10vw",
+  };
   return (
     <div>
       <ListingPagesTopBreifComp
@@ -17,8 +21,9 @@ function ShowsPage() {
         ]}
         bgELementsRatios={[1, 0.9, 0.55, 0.45]}
         primaryHeight="50vh"
-        topicTitle="Shows"
+        topicTitle="SHOWS"
         topicDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tempus, quis dolor sit amet"
+        textDivStyling={divStyling}
       />
       {/* <Graphic1 /> */}
     </div>
