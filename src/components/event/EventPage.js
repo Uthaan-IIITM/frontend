1da84536c1feb2850bedc2282a91a6483c84c503
Raj varsani <varasaniraj5@gmail.com>
2021-09-02 14:56:02 +0530

    Added events upper section component in events page
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/event/EventPage.js
+++ src/components/event/EventPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import EventLowerSection from "./lower_section/EventLowerSection";
+import EventsUpperSection from "./upper_section/EventsUpperSection";
 
 function EventPage() {
   return (
     <div>
+      <EventsUpperSection/>
       <EventLowerSection />
     </div>
   );
