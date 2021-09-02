d22dc11c6566b4bbbc2b8af3bb4331e25dca6e9b
Raj varsani <varasaniraj5@gmail.com>
2021-09-02 18:54:34 +0530

    Added ListingPagesSmoothScrollContainer in EventPage
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/event/EventPage.js
+++ src/components/event/EventPage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import EventLowerSection from "./lower_section/EventLowerSection";
 import EventsUpperSection from "./upper_section/EventsUpperSection";
+import ListingPagesSmoothScrollContainer from "./../_general/listing_pages_top_bottom_comps/ListingPagesSmoothScrollContainer";
 
 function EventPage() {
   return (
     <div>
-      <EventsUpperSection/>
-      <EventLowerSection />
+      <ListingPagesSmoothScrollContainer
+        topContainer={[<EventsUpperSection />]}
+        bottomContainer={<EventLowerSection />}
+      />
     </div>
   );
 }
