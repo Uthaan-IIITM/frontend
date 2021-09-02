5975918a14e6cef3eace107ead085db2739b4c62
Raj varsani <varasaniraj5@gmail.com>
2021-09-02 18:47:27 +0530

    Updated eventLowerSection.js >> Added Prevention for child elements in window scroll
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/event/lower_section/EventLowerSection.js
+++ src/components/event/lower_section/EventLowerSection.js
@@ -9,32 +9,36 @@ function EventLowerSection() {
     window.addEventListener("scroll", () => {
       let childElements = document.getElementsByClassName(
         "event-lower-section-wrapper"
-      )[0].childNodes;
+      )[0]?.childNodes;
 
-      let elemHeight = parseFloat(
-        getComputedStyle(childElements[0]).getPropertyValue("height")
-      );
+      if (childElements) {
+        let elemHeight = parseFloat(
+          getComputedStyle(childElements[0]).getPropertyValue("height")
+        );
 
-      let firstChildElemPosition = childElements[0].getBoundingClientRect().top;
+        let firstChildElemPosition =
+          childElements[0].getBoundingClientRect().top;
 
-      let positionDiffrenceBetweenFirstElemAndCenter =
-        window.innerHeight / 2 - firstChildElemPosition;
+        let positionDiffrenceBetweenFirstElemAndCenter =
+          window.innerHeight / 2 - firstChildElemPosition;
 
-      let activePositionIdexForLine = Math.ceil(
-        positionDiffrenceBetweenFirstElemAndCenter / elemHeight
-      );
+        let activePositionIdexForLine = Math.ceil(
+          positionDiffrenceBetweenFirstElemAndCenter / elemHeight
+        );
 
-      let lineDiv = document.getElementsByClassName(
-        "events-timeline-filled-line"
-      )[0];
-      let lineDivHeight =
-        (3 * window.innerWidth) / 100 + activePositionIdexForLine * elemHeight;
+        let lineDiv = document.getElementsByClassName(
+          "events-timeline-filled-line"
+        )[0];
+        let lineDivHeight =
+          (3 * window.innerWidth) / 100 +
+          activePositionIdexForLine * elemHeight;
 
-      if (activePositionIdexForLine == childElements.length - 1) {
-        lineDivHeight = lineDivHeight + (3 * window.innerWidth) / 100;
+        if (activePositionIdexForLine == childElements.length - 1) {
+          lineDivHeight = lineDivHeight + (3 * window.innerWidth) / 100;
+        }
+
+        lineDiv.style.height = lineDivHeight + "px";
       }
-
-      lineDiv.style.height = lineDivHeight + "px";
     });
   }, []);
 
