c5a619850ae4647b20e7b02c37d4ac0afc7486c0
Akash Gupta <akash.gupta.anup@gmail.com>
2021-09-07 14:21:19 +0530

    Default state for slidingIndex
    Co-authored-by: Raj Varsani <RajVarsani@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar slidingIndex={1} />
+        <Navbar slidingIndex={0} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route
