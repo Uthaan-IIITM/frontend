9e89e8b19c39c3bfd6a0bc7bb5cfeebb557696b8
Raj varsani <varasaniraj5@gmail.com>
2021-09-08 15:28:12 +0530

    Added clokk shadow img in BridgingGapBetweenJuniorsAndSeniorsCopy
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/home/upper-section/illustration-components/BridgingGapBetweenJuniorsAndSeniors_copy.js
+++ src/components/home/upper-section/illustration-components/BridgingGapBetweenJuniorsAndSeniors_copy.js
@@ -83,6 +83,7 @@ function BridgingGapBetweenJuniorsAndSeniorsCopy({ containerWidth = "75vw" }) {
         id="BGBJAS-laptop"
       />
       <img src={images["stool.svg"].default} alt="graphic" id="BGBJAS-stool" />
+      <img src={images["clock_shadow.svg"].default} alt="graphic" id="BGBJAS-clock-shdow" />
 
       <BgbjasClock clockContainerWidth={containerWidth} />
       <BgbjasRightLeaves rightLeavesContainerWidth={containerWidth} />
