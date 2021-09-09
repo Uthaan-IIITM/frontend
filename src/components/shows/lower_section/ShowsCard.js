d819399e76570458bd1f073dfe4bd85560120ca0
Akash Gupta <akash.gupta.anup@gmail.com>
2021-09-09 11:14:31 +0530

    Added Image wrapper in show cards

--- src/components/shows/lower_section/ShowsCard.js
+++ src/components/shows/lower_section/ShowsCard.js
@@ -4,7 +4,13 @@ import PropTypes from "prop-types";
 import "../../../styles/shows/lower_section/shows_card.css";
 
 function ShowsCard({ cardTitle, cardDate, cardDescription, cardImage }) {
-  return <div className="shows-card-main-wrapper"></div>;
+  return (
+    <div className="shows-card-main-wrapper">
+      <div className="shows-card-image-wrapper">
+        <img src={cardImage} alt="shows-card-image" />
+      </div>
+    </div>
+  );
 }
 
 ShowsCard.propTypes = {
