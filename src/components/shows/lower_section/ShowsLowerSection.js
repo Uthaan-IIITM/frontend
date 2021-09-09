90c5eb6a51605d9730ed71c05dc46186a5f8d9ae
Akash Gupta <akash.gupta.anup@gmail.com>
2021-09-09 11:43:52 +0530

    Minor formatting

--- src/components/shows/lower_section/ShowsLowerSection.js
+++ src/components/shows/lower_section/ShowsLowerSection.js
@@ -24,7 +24,8 @@ function ShowsLowerSection() {
         <ShowsCard
           cardTitle="Alfaaz - Season 2 | Ft. Nitish Upadhyay - 'Ek Jigyasu Chitrakar' | Episode 3 | Uthaan | IIIT Gwalior"
           cardDate="1 Aug 2021"
-          cardDescription={`We present to you "Ek Jigyasu Chitrakar" Mr. Nitish Upadhyay, portraying his art's elegance and soul captivating beauty through his words which are not enough to describe the ravishing and tantalizing art that he created. `}
+          cardDescription={`We present to you "Ek Jigyasu Chitrakar" Mr. Nitish Upadhyay, portraying his art's elegance and soul captivating beauty through his words which are not enough to describe the ravishing and tantalizing art that he created.
+           `}
           cardImage={roughPhoto}
         />
       </div>
