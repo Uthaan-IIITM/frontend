0b286457b5796c7581ff0123ad9bb4b98bea5534
Raj varsani <varasaniraj5@gmail.com>
2021-09-21 21:27:24 +0530

    Updated ShowsLowerLeftSubSection >> Added soooth hovering effect
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/shows/lower_section/left_sub_sec/ShowsLowerLeftSubSection.js
+++ src/components/shows/lower_section/left_sub_sec/ShowsLowerLeftSubSection.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import reactDom from "react-dom";
 import "../../../../styles/shows/lower_section/left_sub_sec/shows_lower_left_sub_sec.css";
 import ShowsLowerLeftSubSectionTitleComp from "./ShowsLowerLeftSubSectionTitleComp";
 
-function ShowsLowerLeftSubSection({ showsTitlesList }) {
+import calculateIndicesAccToActiveIndexForNav from "./helpers/indicesCalculatorAccToActiveIndex";
+import formatNumber from "../../../_general/helpers/nemerical_formatter";
+
+function ShowsLowerLeftSubSection({ showsTitlesList = [] }) {
+  const titlesListWrapperRef = useRef(123);
+
+  const [titlesStateIndices, setTitlesStateIndices] = useState([]);
+  const [currentActiveDisplayListPosIndex, setCurrentActiveListPosIndex] =
+    useState(0);
+
+  const handleListHover = (e) => {
+    let container = reactDom.findDOMNode(titlesListWrapperRef.current);
+    let containerProps = reactDom
+      .findDOMNode(titlesListWrapperRef.current)
+      .getBoundingClientRect();
+    let hoverStateIndex = e.clientY - containerProps.top;
+    console.log(
+      Math.floor(
+        (hoverStateIndex / containerProps.height) * showsTitlesList.length
+      )
+    );
+    calculateStatesForList(
+      (hoverStateIndex / containerProps.height) * showsTitlesList.length
+    );
+  };
+
+  const calculateStatesForList = (index) => {
+    // setCurrentActiveListPosIndex(index);
+    setTitlesStateIndices(
+      // formatNumber(
+      calculateIndicesAccToActiveIndexForNav(index, showsTitlesList.length)
+      // 1
+      // )
+    );
+  };
+
   return (
-    <div className="shows-lower-left-sec-primary-wrapper">
+    <div
+      className="shows-lower-left-sec-primary-wrapper"
+      onMouseMove={(e) => {
+        handleListHover(e);
+      }}
+      ref={titlesListWrapperRef}
+    >
       {showsTitlesList.map((showsTitle, index) => {
         return (
-          <div className="shows-lower-left-title-comp-wrapper" key={index}>
-            <ShowsLowerLeftSubSectionTitleComp />
-          </div>
+          <span className="shows-lower-left-title-comp-wrapper" key={index}>
+            <ShowsLowerLeftSubSectionTitleComp
+              title={showsTitle}
+              opacityIndex={titlesStateIndices[index]}
+              scaleIndex={titlesStateIndices[index] * 0.7 + 0.3}
+            />
+          </span>
         );
       })}
     </div>
