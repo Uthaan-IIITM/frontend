a7974bb8de1651aa0d484e219e997881424b39bf
Raj varsani <varasaniraj5@gmail.com>
2021-09-27 20:59:05 +0530

    UPdated ListingPagesTopBreifComp >> Added graphic component
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/_general/listing_pages_top_bottom_comps/ListingPagesTopBreifComp.js
+++ src/components/_general/listing_pages_top_bottom_comps/ListingPagesTopBreifComp.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import "../../../styles/_general/listing_pages_top_bottom_comps/listing_pages_top_breif_comp.css";
 import "../../../styles/_general/listing_pages_top_bottom_comps/listing_pages_top_breif_comp_events_page.css";
+import "../../../styles/_general/listing_pages_top_bottom_comps/listing_pages_top_breif_comp_shows_page.css";
 import formatNumber from "../helpers/nemerical_formatter";
 import TextComponent from "../TextComponent";
 
@@ -13,6 +14,8 @@ function ListingPagesTopBreifComp({
   textDivStylingClass,
   textTitleStylingClass,
   textDescriptionStylingClass,
+  primaryGraphicCompStylingClass,
+  PrimaryGraphicComp,
 }) {
   const extremeBgPositionValues = useRef({
     xInitialExtremes: ["11%", "10%", "-15%", "-17%"],
@@ -21,15 +24,28 @@ function ListingPagesTopBreifComp({
     yInitialExtremes: ["-1.5%", "-1.3%", "0%", "0%"],
   });
 
+  const primaryGraphicExtremePositions = useRef({
+    xInitialExtreme: "1%",
+    xEndExtreme: "-0.5%",
+    yEndExtreme: "-1%",
+    yInitialExtreme: "1%",
+  });
+
   const [bgElementsPositionValues, setBgElementsPositionValues] = useState({
     xPos: ["0px", "0px", "0px", "0px"],
     yPos: ["0px", "0px", "0px", "0px"],
   });
-  const [lastTimeHeheh, setLastTimeHeheh] = useState(Date.now());
+  const [lastTimeLayoutUpdationTime, setLastTimeLayoutUpdationTime] = useState(
+    Date.now()
+  );
+  const [primaryGarphicPosValues, setPrimaryGarphicPosValues] = useState({
+    vertical: 0,
+    horizontal: 0,
+  });
 
   const handleMouseMovement = (e) => {
-    if (Date.now() - lastTimeHeheh > 100) {
-      setLastTimeHeheh(Date.now());
+    if (Date.now() - lastTimeLayoutUpdationTime > 100) {
+      setLastTimeLayoutUpdationTime(Date.now());
       let tempBgDivPositions = { ...bgElementsPositionValues };
       let xFractionIndex = formatNumber(
         e.clientX / document.body.clientWidth,
@@ -51,6 +67,18 @@ function ListingPagesTopBreifComp({
         tempBgDivPositions.xPos[i] = tempElemXPos;
       }
       setBgElementsPositionValues(tempBgDivPositions);
+      setPrimaryGarphicPosValues({
+        horizontal: `calc( (${xFractionIndex} * ${
+          primaryGraphicExtremePositions.current.xEndExtreme
+        }) +
+        (${1 - xFractionIndex} *
+          ${primaryGraphicExtremePositions.current.xInitialExtreme}))`,
+        vertical: `calc( (${yFractionIndex} * ${
+          primaryGraphicExtremePositions.current.yEndExtreme
+        }) +
+        (${1 - yFractionIndex} *
+          ${primaryGraphicExtremePositions.current.yInitialExtreme}))`,
+      });
     }
   };
 
@@ -80,6 +108,15 @@ function ListingPagesTopBreifComp({
             );
           })}
       </div>
+      <div
+        className={primaryGraphicCompStylingClass}
+        style={{
+          transform: `translate(${primaryGarphicPosValues.horizontal} , ${primaryGarphicPosValues.vertical})`,
+        }}
+      >
+        {PrimaryGraphicComp}
+      </div>
+
       <div className={textDivStylingClass}>
         {/* <TextComponent
           innerContent={topicTitle}
