1628e4715e6cb7dd1131175c78b51975470070fe
Raj varsani <varasaniraj5@gmail.com>
2021-09-28 21:30:00 +0530

    Removed garbage code form ShowsPage comp
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/shows/ShowsPage.js
+++ src/components/shows/ShowsPage.js
@@ -1,28 +1,9 @@
 import React from "react";
-import ListingPagesTopBreifComp from "../../components/_general/listing_pages_top_bottom_comps/ListingPagesTopBreifComp";
 import ShowsLowerSection from "./lower_section/ShowsLowerSection";
 import ShowsUpperSection from "./upper_section/ShowsUpperSection";
 import ListingPagesSmoothScrollContainer from "./../_general/listing_pages_top_bottom_comps/ListingPagesSmoothScrollContainer";
 
 function ShowsPage() {
-  let divStyling = {
-    top: "21vh",
-    left: "10vw",
-  };
-  let textTitleStyle = {
-    fontFamily: "Manrope",
-    fontSize: "4vw",
-    fontWeight: "800",
-    letterSpacing: "0.3em",
-  };
-
-  let textDescriptionStyle = {
-    fontFamily: "Manrope",
-    fontSize: "1.5vw",
-    fontWeight: "500",
-    width: "40vw",
-  };
-
   return (
     <div>
       <div>
