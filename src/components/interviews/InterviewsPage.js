d050b8bc0be0ab2fc04bb8a5454c811d66527825
Raj <varasaniraj5@gmail.com>
2021-10-09 14:26:34 +0530

    Used InterviewsLowerSection in  InterviewsPage
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/interviews/InterviewsPage.js
+++ src/components/interviews/InterviewsPage.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ListingPagesSmoothScrollContainer from './../_general/listing_pages_top_bottom_comps/ListingPagesSmoothScrollContainer';
-import ShowsLowerSection from './../shows/lower_section/ShowsLowerSection';
-import InterviewsUpperSection from './upper_section/InterviewsUpperSection';
+import ListingPagesSmoothScrollContainer from "./../_general/listing_pages_top_bottom_comps/ListingPagesSmoothScrollContainer";
+import InterviewsUpperSection from "./upper_section/InterviewsUpperSection";
+import InterviewsLowerSection from "./lower_section/InterviewsLowerSection";
 
 function InterviewsPage() {
   return (
     <div>
       <ListingPagesSmoothScrollContainer
         topContainer={[<InterviewsUpperSection />]}
-        bottomContainer={<ShowsLowerSection />}
+        bottomContainer={<InterviewsLowerSection />}
       />
     </div>
   );
