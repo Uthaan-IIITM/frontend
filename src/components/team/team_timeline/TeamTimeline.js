b10d8d37fe4ca45815746736b3a01c99d09292e1
Raj <varasaniraj5@gmail.com>
2021-10-09 21:10:06 +0530

    Created  TeamTimeline
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

---
+++ src/components/team/team_timeline/TeamTimeline.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../../styles/team/team_timeline/team_timeline.css";
+import TeamIndividualProfileComp from "./TeamIndividualProfileComp";
+
+function TeamTimeline({ timelineData }) {
+  return (
+    <div className="team-timeline-primary-wrapper">
+      {timelineData?.map((item, index) => {
+        return (
+          <div className="team-timeline-item-wrapper" key={index}>
+            <TeamIndividualProfileComp />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default TeamTimeline;
