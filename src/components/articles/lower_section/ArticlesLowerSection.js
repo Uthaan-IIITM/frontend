db4e6da7891625b112cd3524cd74be101dcc6141
Raj <varasaniraj5@gmail.com>
2021-10-09 14:11:47 +0530

    Added sorting functionality in ArticlesLowerSection
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/articles/lower_section/ArticlesLowerSection.js
+++ src/components/articles/lower_section/ArticlesLowerSection.js
@@ -7,31 +7,48 @@ import "../../../styles/articles/articles_lower_sec.css";
 import ArticlesAndInterviewsDataObj from "../../_general/helpers/articles_and_interviews_data_constructor";
 import SortingComp from "../../_general/SortingComp";
 
+import convertTsToDate from "../../_general/helpers/dateConverter";
+import sortDataBy from "../../_general/helpers/articles_and_interviews_sorter";
+
 function ArticlesLowerSection() {
   const [articlesData, setArticlesData] = useState([]);
 
-  useEffect(() => {
+  useEffect(async () => {
     let tempData = [];
     for (let i = 0; i < 30; i++) {
       tempData.push(
         new ArticlesAndInterviewsDataObj(
           "0*ndy6ybdngG_h48BA",
-          "An Interview with the International Dance Team of Sri Lanka",
-          "Jan 13, 2020",
+          `${
+            i + 1
+          } An Interview with the International Dance Team of Sri Lanka`,
+          Date.now(),
+          convertTsToDate(Date.now()),
           "https://www.google.com/"
         )
       );
+      await sleep(10);
     }
     setArticlesData(tempData);
   }, []);
 
   function handlSortChange(newSortState) {
-    console.log(newSortState);
+    setArticlesData(
+      sortDataBy(
+        articlesData,
+        "timeStamp",
+        newSortState === "NEWEST_FIRST" ? 1 : -1
+      )
+    );
+  }
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   return (
     <div className="articles-lower-section-primary-wrapper">
-      <SortingComp />
+      <SortingComp onSortChangeFun={handlSortChange} />
       <ArticlesAndInterviewsGrid inputData={articlesData} />
     </div>
   );
