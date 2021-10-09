20ca7039f1ac01a70c766c9b8ef5b3f8763571ea
Raj <varasaniraj5@gmail.com>
2021-10-09 14:25:21 +0530

    Removed garbage imports from ArticlesLowerSection
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/articles/lower_section/ArticlesLowerSection.js
+++ src/components/articles/lower_section/ArticlesLowerSection.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "../../../styles/_general/articles_and_interviews_lower_sec.css";
-
 import ArticlesAndInterviewsLowerSection from "../../_general/ArticlesAndInterviewsLowerSection";
 
 function ArticlesLowerSection() {
