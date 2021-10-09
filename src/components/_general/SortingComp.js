4216d53c069cf5730fbce503fdc2443b07b49d7a
Raj <varasaniraj5@gmail.com>
2021-10-09 13:14:20 +0530

    Updated sorting comp
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/_general/SortingComp.js
+++ src/components/_general/SortingComp.js
@@ -1,16 +1,46 @@
-import React from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect, useRef } from "react";
 
 import "../../styles/_general/sorting_comp.css";
 
-function SortingComp() {
-  const [currentSort, setCurrentSort] = React.useState("DATE");
+function SortingComp({ onSortChangeFun }) {
+  const stateNames = useRef({
+    NEWEST_FIRST: "Newest First",
+    OLDEST_FIRST: "Oldest First",
+  });
+
+  const [currentSort, setCurrentSort] = useState("NEWEST_FIRST");
+  useEffect(() => {
+    if (onSortChangeFun) {
+      onSortChangeFun(currentSort);
+    }
+  }, [currentSort]);
+
+  function updateSortState(e) {
+    e.target.style.opacity = 0;
+    if (currentSort === "NEWEST_FIRST") {
+      setCurrentSort("OLDEST_FIRST");
+    } else {
+      setCurrentSort("NEWEST_FIRST");
+    }
+    setTimeout(() => {
+      e.target.style.opacity = 1;
+    }, 100);
+  }
+
   return (
-    <div className="sorting-comp-primary-div">
-      {/* <div className="sorting-comp-secondary-div"> */}
-      <h5 className="sorting-title">Sort By:</h5>
-      <h6 className="sorting-state">{currentSort}</h6>
-      {/* </div> */}
+    <div className="sorting-comp-primary-wrapper">
+      <div
+        className="sorting-comp-secondary-wrapper"
+        onClick={(e) => {
+          updateSortState(e);
+        }}
+      >
+        <h5 className="sorting-comp-title">Sort By : </h5>
+        <h6 className="sorting-comp-sorting-state">
+          {stateNames.current[currentSort]}
+        </h6>
+      </div>
+      <hr />
     </div>
   );
 }
