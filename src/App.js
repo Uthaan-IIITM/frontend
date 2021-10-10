3797e8f58b027769491b579cc295e39bf8db2f93
Raj <varasaniraj5@gmail.com>
2021-10-10 18:17:03 +0530

    Created route for TeamPage
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ArticlesPage from "./components/articles/ArticlesPage";
 import InterviewsPage from "./components/interviews/InterviewsPage";
 import ArticlesAndInterviewsCard from "./components/_general/ArticlesAndInterviewsCard";
 import ArticlesLowerSection from "./components/articles/lower_section/ArticlesLowerSection";
+import TeamPage from './components/team/TeamPage';
 
 function App() {
   const [state, dispatch] = useStateValue();
@@ -27,6 +28,7 @@ function App() {
           <Route exact path="/shows" component={ShowsPage} />
           <Route exact path="/articles" component={ArticlesPage} />
           <Route exact path="/interviews" component={InterviewsPage} />
+          <Route exact path="/team" component={TeamPage} />
           <Route exact path="/123" component={ArticlesLowerSection} />
         </Switch>
       </Router>
