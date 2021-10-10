fa0d62da77524fce2cc852d611d259cd386056a2
Raj <varasaniraj5@gmail.com>
2021-10-10 18:16:42 +0530

    Added teamPageColorThemes data in utils
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
+import TeamIndividualColorThemeObj from "./components/team/team_timeline/helpers/team_individual_profile_color_theme_obj_constructor";
+
 export const mediumPostImageURL = "https://miro.medium.com/max/500/";
+
+export const teamPageColorThemes = [
+  new TeamIndividualColorThemeObj("#44C4FF", "#F5FBFF", "#80D7EA"),
+  new TeamIndividualColorThemeObj("#5044FF", "#F6F5FF", "#8081EA"),
+  new TeamIndividualColorThemeObj("#504485", "#F5F0FA", "#9266D9"),
+  new TeamIndividualColorThemeObj("#FF44A0", "#FFF5F9", "#EA80C1"),
+  new TeamIndividualColorThemeObj("#FF445B", "#FFF5F5", "#EA809B"),
+];
