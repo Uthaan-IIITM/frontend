585bfc657f71358efaccc57332afe88876363a30
Raj <varasaniraj5@gmail.com>
2021-10-13 14:16:16 +0530

    Created Contact Us Page Lower sec >> Added related var to utils
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/utils.js
+++ src/utils.js
@@ -23,3 +23,9 @@ export const contactLinks = {
 };
 
 export const contactPhoneNumbers = ["8989738932", "8317057596"];
+
+export const googleMapsEmbedURL =
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10569.301225709418!2d78.1728634891839!3d26.24920269941473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3976c6e5d32a4d53%3A0xf834069adc0c9b89!2sIndian%20Institute%20of%20Information%20Technology%20and%20Management%20Gwalior!5e0!3m2!1sen!2sin!4v1634102179694!5m2!1sen!2sin";
+
+
+
