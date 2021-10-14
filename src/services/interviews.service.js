8ba995f64f7f681a3c6b15b0b37fc0013714c2dc
Raj <varasaniraj5@gmail.com>
2021-10-14 21:25:26 +0530

    Removed garbage logs
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/services/interviews.service.js
+++ src/services/interviews.service.js
@@ -4,7 +4,6 @@ import { INTERVIEWS_DATA_URL } from "../utils/APIs";
 export const interviews = async () => {
   try {
     const { data } = await axios.get(INTERVIEWS_DATA_URL);
-    console.log(data);
     return data;
   } catch (err) {
     throw err;
