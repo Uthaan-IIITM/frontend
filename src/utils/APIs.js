9b3b36315935d1a17d5cc06456e515cca6adf956
Raj <varasaniraj5@gmail.com>
2021-10-15 21:56:27 +0530

    Added shows API URL >> Updated messaging api url name
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/utils/APIs.js
+++ src/utils/APIs.js
@@ -1,6 +1,7 @@
 export const BASE_URL = "http://localhost:4000/api/";
 
 export const EVENTS_DATA_URL = BASE_URL + "events";
+export const SHOWS_DATA_URL = BASE_URL + "shows";
 export const ARTICLES_DATA_URL = BASE_URL + "articles";
 export const INTERVIEWS_DATA_URL = BASE_URL + "interviews";
-export const CONTACT_US_DATA_URL = BASE_URL + "messages";
+export const CONTACT_US_MESSAGING_URL = BASE_URL + "messages";
