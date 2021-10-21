8f05e4db531b943f961273d59199718062847503
Raj <varasaniraj5@gmail.com>
2021-10-21 21:22:57 +0530

    Made Navbar responsive
    Co-authored-by: Akash Gupta <akashgupta1909@users.noreply.github.com>

--- src/components/_general/Navbar.js
+++ src/components/_general/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
@@ -7,6 +7,26 @@ import "../../styles/_general/Navbar.css";
 import UthaanLogo from "../../assets/uthaan_logo/uthaan_logo.svg";
 
 import { allRoutes } from "./../../utils/GeneralConstants";
+import { useLayoutEffect } from "react";
+import { useState } from "react";
+
+import { Divide as Hamburger } from "hamburger-react";
+import reactDom from "react-dom";
+
+function useMediaQuery() {
+  const [screenSize, setScreenSize] = useState([0, 0]);
+
+  useLayoutEffect(() => {
+    function updateScreenSize() {
+      setScreenSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateScreenSize);
+    updateScreenSize();
+    return () => window.removeEventListener("resize", updateScreenSize);
+  }, []);
+
+  return screenSize;
+}
 
 const NavbarRoutersName = [
   "Home",
@@ -38,8 +58,20 @@ function Navbar({
   marginLeftDefaultValue,
   marginLeftDifferenceValue,
 }) {
+  const hamburgerMenuListRef = useRef(256);
+
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+  const [isNavbarVisible, setIsNavbarVisible] = useState(true);
+
+  const [width] = useMediaQuery();
+
   const { pathname } = useLocation();
 
+  useEffect(() => {
+    if (allRoutes.includes(pathname.split("/")[1]) != isNavbarVisible) {
+      setIsNavbarVisible(allRoutes.includes(pathname.split("/")[1]));
+    }
+  }, [isHamburgerOpen]);
   const NavbarRoutersNameList = NavbarRoutersName.map((router, index) => {
     return (
       <NavLink
@@ -56,10 +88,25 @@ function Navbar({
       </NavLink>
     );
   });
+  const NavbarHamburgerRoutersNameList = NavbarRoutersName.map(
+    (router, index) => {
+      return (
+        <NavLink
+          exact
+          to={NavbarRouters[index]}
+          key={index}
+          activeClassName="navbar-active-class"
+          className="navbar-hamburger-item"
+        >
+          {router}
+        </NavLink>
+      );
+    }
+  );
 
   return (
     <>
-      {allRoutes.includes(pathname.split("/")[1]) ? (
+      {isNavbarVisible ? (
         <nav
           className="navbar-main-wrapper"
           style={{
@@ -67,11 +114,62 @@ function Navbar({
             paddingRight: `calc(${paddingRightDifferenceValue}*${slidingIndex} + ${paddingRightDefaultValue})`,
           }}
         >
-          <a href="/" className="navbar-main-left-wrapper">
-            <img src={UthaanLogo} alt="UthaanLogo" />
-          </a>
-          <div className="navbar-main-right-wrapper">
-            {NavbarRoutersNameList}
+          <div className="navbar-secondary-wrapper">
+            {width > 900 ? (
+              <>
+                <a href="/" className="navbar-main-left-wrapper">
+                  <img src={UthaanLogo} alt="UthaanLogo" />
+                </a>
+                <div className="navbar-main-right-wrapper">
+                  {NavbarRoutersNameList}
+                </div>
+              </>
+            ) : (
+              <>
+                <a href="/" className="navbar-main-left-wrapper">
+                  <img src={UthaanLogo} alt="UthaanLogo" />
+                </a>
+                <div
+                  className="navbar-hamburger-wrapper"
+                  tabIndex={0}
+                  onClick={(e) => setIsHamburgerOpen(true)}
+                  onBlur={(e) => {
+                    setIsHamburgerOpen(false);
+                  }}
+                >
+                  <div>
+                    <Hamburger
+                      toggled={isHamburgerOpen}
+                      toggle={isHamburgerOpen}
+                      size={20}
+                      color="#222222"
+                      label="Show menu"
+                      duration={0.5}
+                      hideOutline={false}
+                    />
+                  </div>
+                  <div
+                    className="navbar-hamburger-links-list-wrapper"
+                    ref={hamburgerMenuListRef}
+                    style={
+                      isHamburgerOpen
+                        ? {
+                            opacity: 1,
+                            height: reactDom.findDOMNode(
+                              hamburgerMenuListRef.current
+                            ).scrollHeight,
+                          }
+                        : {
+                            opacity: 0,
+                            height: 0,
+                          }
+                    }
+                  >
+                    {NavbarHamburgerRoutersNameList}
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </nav>
       ) : null}
